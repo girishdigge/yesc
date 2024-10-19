c1469c510eade81920d7abf2b255cd473ea074a0
girishdigge <girishdigge@gmail.com>
2024-10-19 22:17:14 +0530

    allowed origin 5

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,11 +1,11 @@
 import { allowedOrigin } from './allowedOrigins.js';
 export const corsOptions = {
   origin: (origin, callback) => {
-    // if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
-    //   callback(null, true);
-    // } else {
-    //   callback(new Error('Not allowed by CORS'));
-    // }
+    if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
   },
   credentials: true,
   optionSuccessStatus: 200,
